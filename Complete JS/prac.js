100ea6efbb80ba9945ab796b524014465d1eff68
Hamza Dev <hamzamasroor40@gmail.com>
2025-07-28 20:33:58 +0500

    Add files via upload

---
+++ Complete JS/prac.js
@@ -0,0 +1,95 @@
+// Q1..1
+// var a = 23;
+// a = 25; 
+// console.log(a);
+// Q1..2
+let name= "hamza";
+name = "ali";
+console.log(name);
+// Q2..1
+// const student = {
+//     name :"Hamza",
+//     age : 19,
+//     Grade : "B"
+    
+// }
+// console.log(student);
+// console.log(student);
+// Q2..2
+// console.log(student.Grade = "A");
+// console.log(student["Grade"] = "A+");
+// Q3..1
+// let a = 4;
+// let b = 3;
+
+// console.log("a + b = ",a + b);
+// console.log("a - b = ",a - b);
+// console.log("a * b = ",a * b);
+// console.log("a / b = ",a / b);
+// console.log("a % b = ",a % b);
+// console.log("a ** b = ",a ** b);
+// Q3..2
+// let isloggedIn = true;
+// let isAdmin = false;
+
+// if(isloggedIn && isAdmin){
+//     console.log("user is sign in");
+// }else{
+//     console.log("user is not sign in");
+// }
+// let isloggedIn = true;
+// let isAdmin = false;
+
+// if(isloggedIn || isAdmin){
+//     console.log("user is sign in");
+// }else{
+//     console.log("user is not sign in");
+// }
+// Q4..1
+// let age = prompt("what is your age?");
+// age = Number(age)
+// if(age >=18 && age <=60){
+//     console.log("Adult"); 
+// }else if(age >=60){
+//     console.log("OLD")
+// }else{
+//     console.log("not valid")
+// }
+// console.log(age);
+// Q4..2
+// let num = prompt("enter the num to if its even or odd");
+// num = Number(num);
+// if(num % 2=== 0){
+//     console.log("EVEN")
+// }else{
+//     console.log("ODD")
+// }
+// console.log(num)
+// Q5..1
+// let a ="hamza";
+// let b =true;
+// let c =19;
+// let d =BigInt(12334343478384686);
+// let e =undefined;
+// console.log(a);
+// console.log(typeof a);
+// console.log(b);
+// console.log(typeof b);
+// console.log(c);
+// console.log(typeof c);
+// console.log(d);
+// console.log(typeof d);
+// console.log(e);
+// console.log(typeof e);
+// Q5..2
+// let user = prompt("IS JS FUN yes or no");
+// user = user === "yes" ? true : false;
+
+// if (user) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+// console.log(user); 
+
+
