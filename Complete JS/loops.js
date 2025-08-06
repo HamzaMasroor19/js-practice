774fe657ebd9a07b2b4df179cd46cc39baa61c51
Hamza Dev <hamzamasroor40@gmail.com>
2025-08-06 20:05:10 +0500

    Add files via upload

---
+++ Complete JS/loops.js
@@ -0,0 +1,73 @@
+// let sum = 0;
+// let n = prompt("Enter the no");
+// n = Number(n);
+
+// for(let i = 0;i < n;i++){
+//     sum += i+1;
+//     console.log("hello",sum )
+
+// }
+// let sum = 1;
+// let n = prompt("enter the number");
+// n =Number(n);
+// for(let i =1;i <=n;i++){
+// sum *=(i);
+// console.log(sum);
+// }
+
+
+// for(let i = 1;i< 10;i++){
+//     console.log(i * 2 )
+// }
+// let i = 0;
+// while(i <10){
+// console.log(i);
+// i++
+// }
+//Q1
+// for(let i = 1;i<=10;i++){
+//     console.log(i);
+// }
+//Q2
+// for(let i = 1;i < 11;i++){
+//     console.log(i * 2)
+// }
+//Q3
+// let sum = 0;
+// let n = prompt("Enter your num");
+// for(let i = 1; i < n;i++){
+// sum += i;
+// console.log(sum)
+// }
+//Q4 
+// let num =prompt("enter the number");
+// for(let i=1; i<=10;i++){
+//     console.log(num +"x"+i+"="+i*num)
+// }
+// Q5
+let multi = 1;
+let n = prompt("Enter your num");
+
+for(let i = 1;i <=n;i++){
+multi *=i;
+}
+console.log(multi);
+//WHILE LOOP
+//Q6
+// let i = 10;
+// while(i>=1){
+//     console.log(i);
+//     i--
+// }
+// let number = prompt("Enter a number (0 to stop):");
+// number = Number(number);
+
+// while (number !== 0) {
+//     console.log("You entered:", number);
+
+//     // Ask again
+//     number = prompt("Enter another number (0 to stop):");
+//     number = Number(number);
+// }
+
+// console.log("You entered 0. Loop ended.");
