2016886ff2def9a8c371143260b5584116de9f04
Hamza Dev <hamzamasroor40@gmail.com>
2025-09-06 20:30:36 +0500

    Update loops.js

--- Complete JS/loops.js
+++ Complete JS/loops.js
@@ -71,3 +71,80 @@ console.log(multi);
 // }
 
 // console.log("You entered 0. Loop ended.");
+//Q1
+// for(let i =1;i<=15;i++){
+//     console.log(i)
+// }
+//Q2
+// for(let i =1;i<=15;i++){
+//     console.log(i *2)
+// }
+//Q3
+// for(let i =1;i<=10;i++){
+//     console.log(`9 X ${i} = ${i * 9}`)
+// }
+//Q4
+// let sum=0;
+// for(let i =1;i<=10;i++){
+//     sum = sum + i;
+// }
+// console.log(sum)
+// for(let row =1;row<=5;row++){
+//     let star ="";
+//     for(col =1;col<=row;col++){
+//       star +="*";  
+//     }
+// console.log(star)
+// }
+// for(row =1;row<=4;row++){
+//     let star="";
+//     for(col=1;col<=6;col++){
+//         star = star +"*"
+//     }
+//     console.log(star)
+// }
+// for(row =1;row<=5;row++){
+//     let star="";
+//     for(col=1;col<=row;col++){
+//         star = star +"*"
+//     }
+//     console.log(star)
+// }
+// for(row =1;row<=5;row++){
+//     let star="";
+//     for(col=5;col>=row;col--){
+//         star = star +"*"
+//     }
+//     console.log(star)
+// }
+//Q1
+// for(let i =1;i <=20;i++){
+//     console.log(i)
+// }
+//Q2
+// for(let i =1;i<=25;i++){
+//     console.log(i*2)
+// }
+// Q3
+// for(let i =1;i<=10;i++){
+//     console.log(`7 X ${i} = ${i * 7}`)
+// }
+//Q4 
+// let sum =0;
+// for(let i =1;i<=100;i++){
+//     sum =sum + i;
+// }
+// console.log(sum)
+//Q5
+// let facter=4;
+// let n = facter;
+// for(let i =1;i<=n;i++){
+// facter =facter * i;
+// }
+// console.log(facter)
+//Q7
+// for(let i =5;i>=1;i--){
+//     console.log(i)
+// }
+
+
