1272c322ca917da395e70e8b34297fe94e367d0b
Hamza Dev <hamzamasroor40@gmail.com>
2025-09-08 01:38:20 +0500

    Update array.js

--- Complete JS/array.js
+++ Complete JS/array.js
@@ -3,3 +3,29 @@ let marks =[98,23,32,45,64];
 for(let i =0;i <=marks.length;i++){
     console.log(marks[i])
 }
+//T1 
+//let num =[1 ,2 ,3 ,4 ,5];
+// console.log(num[0])
+// console.log(num[4])
+//T2
+// let fruit =["Apple","Banana","Mango"];
+// fruit.push("Watermelon")
+// fruit.shift()
+// console.log(fruit)
+//T3
+// let friend =["ali","ahmed","ayaan"]
+// friend[1]="hamza";
+// console.log(friend)
+//T4 
+let num =[2,3,4,5,6,7,8,9,10]
+for(let i =0;i<num.length;i++){
+    console.log(num[i])
+    if(num[i] % 2===0){
+        console.log(`even num: ${num[i]}`)
+    }
+}
+//t5
+// let city =["Karachi","Quetta","KPK"];
+// city.unshift("Lahore");
+// city.push("Hyderabad");
+// console.log(`the total number of city is :${city.length} and the final arry is :${city}`)
