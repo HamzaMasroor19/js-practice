582d6707814b2c517060cf0f879390777ec717fd
Hamza Dev <hamzamasroor40@gmail.com>
2025-09-10 23:44:02 +0500

    Update DOM.js

--- Complete JS/DOM.js
+++ Complete JS/DOM.js
@@ -1,2 +1,26 @@
-let title =document.getElementById("heading")
-console.log(title)
+
+// let title =document.getElementById("heading")
+// console.log(title)
+// let h2 =document.getElementById("heading");
+// let btn =document.querySelector("button");
+// btn.onclick =function(){
+
+//     h2.innerHTML ="Welcome Hamza";
+// }
+
+// console.log(h2)
+let h2 =document.getElementById("count")
+let plus =document.querySelector("button");
+let minus =document.querySelector("button");
+
+let count=0;
+plus.onclick = function(){
+    count =count + 1;
+    display.innerHTML =count
+    
+}
+minus.onclick=function(){
+    count = count - 1;
+    display.innerHTML =count
+}
+console.log(h2)
