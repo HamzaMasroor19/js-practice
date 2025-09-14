bc16eadc5f28d268495c01904f24db6452f21aa8
Hamza Dev <hamzamasroor40@gmail.com>
2025-09-14 20:12:06 +0500

    Update async.js

--- Complete JS/async.js
+++ Complete JS/async.js
@@ -1,12 +1,42 @@
-// async function example
-async function fetchData() {
-    return "Hamza is learning async/await 🚀";
+function sayhello(){
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve("Hello Hamza");
+        },2000)
+    })
 }
-
-// using await
-async function showData() {
-    let message = await fetchData();
-    console.log(message);
+async function show() {
+    let say = await sayhello();
+    console.log(say);
 }
+show()
 
-showData();
+
+// function doubleNum(num) {
+//     let promise = new Promise((resolve,) => {
+//         setTimeout(() => {
+//             resolve(num * 2)
+//         }, 1500);
+//     })
+//     return promise
+// }
+// async function run() {
+//     let newNum = await doubleNum(5);
+//     console.log(newNum)
+// }
+// run()
+function getUser() {
+    let user="Hamza";
+    let age =19;
+    let promise=new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve(`User: ${user} and Age : ${age}`)
+        },2000)
+    })
+    return promise
+}
+async function run(){
+    let run =await getUser();
+    console.log(run)
+}
+run()
