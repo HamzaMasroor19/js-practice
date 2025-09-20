ad88ef37dfc65439d172876abf889ae18831995c
Hamza Dev <hamzamasroor40@gmail.com>
2025-09-20 17:40:26 +0500

    Add files via upload

---
+++ Complete JS/event.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener("click",()=>{
+    alert("Event ")
+})
+let btn2 = document.getElementById("btnSec");
+btn2.onclick =()=>{
+
+    console.log("YES BROTHER")
+}
