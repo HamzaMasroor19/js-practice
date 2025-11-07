d8e9f397dc68c24f850712c128b9a2c1ce401bb3
Hamza Dev <hamzamasroor40@gmail.com>
2025-11-07 23:34:41 +0500

    Update event.js

--- Complete JS/event.js
+++ Complete JS/event.js
@@ -7,3 +7,17 @@ btn2.onclick =()=>{
 
     console.log("YES BROTHER")
 }
+
+
+
+<script>
+  let h1 = document.getElementById("heading");
+
+  h1.addEventListener("mouseover", function() {
+    h1.style.color = "red";
+  });
+
+  h1.addEventListener("mouseout", function() {
+    h1.style.color = "black";
+  });
+</script>
